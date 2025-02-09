3020594c4af048bf651a76335ecfd913a2056112
Shrestha Roy <shretharoy787@gmail.com>
2025-02-10 00:31:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".categories");
+const laptop = document.querySelector('.laptop');
+const camera = document.querySelector('.camera');
+const shoes = document.querySelector('.shoes');
+console.log();
